17417b8a7790cb4ae806bc93a99354696b360554
rgero <rgero@student.21-school.ru>
2019-11-10 13:24:01 +0300

    2

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,49 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 17:46:44 by rgero             #+#    #+#             */
-/*   Updated: 2019/11/10 11:55:58 by rgero            ###   ########.fr       */
+/*   Updated: 2019/11/10 13:20:18 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "ft_printf.h"
+
+char	ft_flag(char c)
+{
+	if (c == '#' || c == '0' || c == '-' || c == ' ' || c == '+' || c == '`')
+		return (c);
+	return (0);
+}
+
+int		ft_width(char **s)
+{
+	int	ret;
+
+	ret = ft_atoi(*s);
+	if (ret > 0)
+		while (*(*s) >= '0' && *(*s) <= '9')
+			(*s)++;
+	return (ret);
+}
+
+t_spec *ft_conversion(char *s)
+{
+	t_spec	*ret;
+
+	if (!(ret = (t_spec *)malloc(sizeof(t_spec))))
+		return (NULL);
+	if ((ret->flag = ft_flag(*s)))
+		s++;
+	printf("ret->flad=%c\n", ret->flag);
+	ret->width = ft_width(&s);
+	printf("ret->width=%i\n", ret->width);
+	printf("after ret->width=%s\n", s);
+	return (ret);
+}
+
 
 
 int ft_printf(const char * restrict format, ...)
@@ -20,6 +56,7 @@ int ft_printf(const char * restrict format, ...)
 	char	*first_agr;
 	int	i;
 	va_list type;
+	t_spec	*spec;
 
 	first_agr = (char *)format;
 	va_start(type, format);
@@ -32,8 +69,14 @@ int ft_printf(const char * restrict format, ...)
 		}
 		else
 		{
-			if (*(++first_agr) == 'i')
+			if (*(++first_agr) == '%')
 			{
+				ft_putchar(*first_agr);
+				++first_agr;
+			}
+			else
+			{
+				spec = ft_conversion(first_agr);
 				i = va_arg(type, int);
 				ft_putnbr(i);
 				++first_agr;
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 17:45:27 by rgero             #+#    #+#             */
-/*   Updated: 2019/11/10 12:34:02 by rgero            ###   ########.fr       */
+/*   Updated: 2019/11/10 13:20:50 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(int args, char **argv)
 	if (args == 2)
 		ft_printf(argv[1], 543);
 
-printf("lib printf c=%i\n", 543);
+printf("lib printf c=%055i\n", 543);
 // printf("Le fichier{cyan}%s{eoc} contient : {red}%s{eoc}", "filename", "str");
 
 	return (0);
