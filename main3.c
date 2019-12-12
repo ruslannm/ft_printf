895889c17ba5c0011b998344f5a8bab1a4671a74
rgero <rgero@student.21-school.ru>
2019-12-12 16:38:48 +0300

    1

--- main3.c
+++ main3.c
@@ -6,7 +6,7 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 15:53:23 by rgero             #+#    #+#             */
-/*   Updated: 2019/12/09 17:41:26 by rgero            ###   ########.fr       */
+/*   Updated: 2019/12/12 16:38:42 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,11 +42,20 @@ union my_double
 int main(void) {
     union floatint u = {10.f};
 	union my_double d = {3.14f};
-	
+	int i;
+	unsigned int z;
+
     printf("%f\n", d.ld);
-    printf("%u\n", d.s_parts.m);
+    printf("mant %u\n", d.s_parts.m);
+    printf("mant %x\n", d.s_parts.m);
     printf("%u\n", d.s_parts.e);
     printf("%o\n", d.s_parts.s);
+	printf("%f\n", d.s_parts.m / 8388608.0);
+	z = d.s_parts.m;
+	i = 0;
+	while (i++ < 23)
+		z = z / 2.;
+    printf("z %u\n", z);
 
     return (0);
 }
--- main5.c
+++ main5.c
@@ -20,7 +20,51 @@ void _print_real (PREALINT num, size_t size)
         printf("%0.4X ", num[size]);
     printf("\n");
 }
+
+void bi_mul(PBIGINT a, PBIGINT b, PBIGINT c)
+{
+    PBIGINT lsda, msda, lsdb, msdb;
+    PBIGINT ptra, ptrb, ptrc, ptrct;
  
+    ULONG carry = 0;
+ 
+    if(BI_GETLEN(a) < BI_GETLEN(b))
+    {
+        ptra = b;
+        ptrb = a;
+    }
+    else
+    {
+        ptra = a;
+        ptrb = b;
+    }
+ 
+    msdb = BI_MSDPTR(ptrb);
+    msda = BI_MSDPTR(ptra);
+ 
+    for(ptrc = BI_LSDPTR(c), lsda = BI_LSDPTR(ptra), lsdb = BI_LSDPTR(ptrb); lsdb <= msdb; ++lsdb, ++ptrc)
+        *ptrc = (USHORT)(carry = (ULONG)*lsda * (ULONG)*lsdb +
+                                 (ULONG)(USHORT)(carry >> BI_BITPERREG));
+    
+    *ptrc = (USHORT)(carry >> BI_BITPERREG);
+ 
+    for(ptrc = BI_LSDPTR(c) + 1, lsda = BI_LSDPTR(ptra) + 1; lsda <= msda; ++lsda, ++ptrc)
+    {
+        carry = 0;
+        for(ptrct = ptrc, lsdb = BI_LSDPTR(ptrb); lsdb <= msdb; ++lsdb, ++ptrct)
+            *ptrct = (USHORT)(carry = (ULONG)*lsda * (ULONG)*lsdb + (ULONG)*ptrct +
+                                      (ULONG)(USHORT)(carry >> BI_BITPERREG));
+ 
+        *ptrct = (USHORT)(carry >> BI_BITPERREG);
+    }
+ 
+    if(carry >> BI_BITPERREG)
+        ++ptrc;
+ 
+    BI_SETLEN(c, ptrc - BI_LSDPTR(c));
+}
+
+
 int main(void)
 {
     REALINT real = { 0xAABB, 0xCCDD, 0xEEFF, 0x1111 };
