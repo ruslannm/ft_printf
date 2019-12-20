a7773b5ada95bec7a01e2641ff7b9f89e1a7e95c
rgero <rgero@student.21-school.ru>
2019-12-20 17:54:01 +0300

    sum dont works

--- ft_bigop.c
+++ ft_bigop.c
@@ -6,13 +6,31 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/13 16:26:38 by rgero             #+#    #+#             */
-/*   Updated: 2019/12/20 16:40:14 by rgero            ###   ########.fr       */
+/*   Updated: 2019/12/20 17:52:44 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 #include "ft_printf.h"
+
+int	ft_strcmp_nb(const char *s1, const char *s2)
+{
+	int	s_len[3];
+
+	s_len[1] = ft_strlen(s1);
+	s_len[2] = ft_strlen(s2);
+	if (s_len[1] > s_len[2])
+		return (1);
+	else if (s_len[1] < s_len[2])
+		return (-1);
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*((unsigned char *)s1) - *((unsigned char *)s2));
+}
 
 static int	ft_get_digit(char *s, int s_len, int i)
 {
@@ -43,7 +61,7 @@ char	*ft_sum(char *s1, char *s2, int base)
 		i++;
 	}
 	if (carry)
-		s[s_len[0] - i] = '1';
+		s[s_len[0] - i] = carry;
 	else
 	{
 		ft_memmove(s, s + 1, s_len[0] - 1);
@@ -51,6 +69,32 @@ char	*ft_sum(char *s1, char *s2, int base)
 	}
 	return (s);
 }
+
+char	*ft_sum_decimal_place(char *s1, char *s2, int base)
+{
+	int 	carry;
+	int		s_len[3];
+	int		i;
+	char	*s;
+
+	s_len[1] = ft_strlen(s1);
+	s_len[2] = ft_strlen(s2);
+	s_len[0] = 2 + (s_len[1] > s_len[2] ? s_len[1] : s_len[2]);
+	carry = 0;
+	if (!(s = ft_strnew(s_len[0] + 1)))
+		return (NULL);
+	i = 1;
+	while (i <= s_len[1] || i <= s_len[2])	
+	{
+		s[s_len[0] - i]  = (ft_get_digit(s1, s_len[1], s_len[0] - i) + ft_get_digit(s2, s_len[2], s_len[0] - i) + carry) % base + '0';
+		carry = (ft_get_digit(s1, s_len[1], s_len[0] - i) + ft_get_digit(s2, s_len[2], s_len[0] - i) + carry) / base;
+		i++;
+	}
+	s[1] = '.';
+	s[0] = carry + '0';
+	return (s);
+}
+
 
 char	*ft_sub(char *s1, char *s2)
 {
@@ -77,7 +121,7 @@ char	*ft_sub(char *s1, char *s2)
 		i++;
 	}
 	i = 0;
-	while (s[i] == '0')
+	while (s[i] == '0' && i < s_len[0] - 1)
 		i++;
 	if (i)
 	{
@@ -108,14 +152,14 @@ char	*ft_mul(char *s1, int y)
 	{
 		carry = 0;
 		i = 1;
-		while (i <= s_len[0])	
+		while (i < s_len[0])	
 		{
 			s[s_len[0] - i]  = (ft_get_digit(s1, s_len[1], i) * y + carry) % 10 + '0';
 			carry = (ft_get_digit(s1, s_len[1], i) * y + carry) / 10;
 			i++;
 		}
 		if (carry)
-			s[s_len[0] - i] = '1';
+			s[s_len[0] - i] = carry + '0';
 		else
 		{
 			ft_memmove(s, s + 1, s_len[0] - 1);
@@ -134,13 +178,13 @@ int 	ft_div_int(char *s1, char *s2)
 	while (ret < 10)
 	{
 		s = ft_mul(s2, ret);
-		if (ft_strcmp(s, s1) > 0)
+		if (ft_strcmp_nb(s, s1) > 0)
 		{
 			ret = ret - 1;
 			free(s);
 			break;
 		}
-		else if (ft_strcmp(s, s1) == 0)
+		else if (ft_strcmp_nb(s, s1) == 0)
 		{
 			free(s);
 			break;
@@ -173,15 +217,15 @@ char *ft_div(char *s1, char *s2, int digit)
 		s1 = ft_sub(s1, tmp);
 //		free (s1);
 //		s1 = tmp2;
-		if (ft_strlen(s1) == 0)
+		if (!ft_strcmp(s1, "0"))
 			break;
 		s1 = ft_mul(s1, 10);
 		d = ft_div_int(s1, s2);
+		if (i == 0)
+			ret[++i] = '.';
 		i++;
-		//if (i == 0)
-		// ret[i] = '.'
 	}
-	return (tmp);
+	return (ret);
 }
 
 int main()
@@ -191,15 +235,18 @@ int main()
 	
 	//ret = NULL;
 	ret = ft_sum("99", "99", 10);
-	printf("res=%s=\n", ret);
-	ret = ft_mul("111111111111111111111111111111", 0);
-	printf("res=%s=\n", ret);
+	printf("res_sum=%s=\n", ret);
+	ret = ft_sum_decimal_place("625", "15", 10);
+	printf("res_sum_decimal_place=%s=\n", ret);
+	
+	ret = ft_mul("16", 7);
+	printf("res_mul=%s=\n", ret);
 	free(ret);
 	tmp = ft_strdup("1");
-    ret = ft_sub(tmp, "0");
-	printf("res=%s=\n", ret);
-	ret = ft_div("1", "16", 100);
-	printf("res=%s=\n", ret);
+    ret = ft_sub(tmp, "1");
+	printf("res_sub=%s=\n", ret);
+	ret = ft_div("1", "9", 100);
+	printf("res_div=%s=\n", ret);
 
 	return (0);
 }
